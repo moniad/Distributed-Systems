1115a6017558413121679ce12dcb431ec1d03a76
moniad <moni.dziedzic@gmail.com>
2020-02-29 00:11:21 +0100

    Added UDP channel. Not sure if it works correctly

--- Lab1/homework/PythonTcpClient.py
+++ Lab1/homework/PythonTcpClient.py
@@ -1,48 +1,72 @@
 import socket
 import threading
+from time import sleep
 
-server_TCP_PORT = 8014
+BUFFER_SIZE = 1024
+
+PORT = 8015
 server_IP = "127.0.0.1"
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.connect((server_IP, server_TCP_PORT))
+tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_server.connect((server_IP, PORT))
 can_receive_messages = False
 
+udp_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-def sender():
-    nickname = input('Give me your nickname: ')
-    server.send(bytes(nickname, 'utf8'))
-    global can_receive_messages
-    can_receive_messages = True
+
+def tcp_sender():
+    tcp_server.send(bytes(nickname, 'utf8'))
 
     while True:
         msg = input()
         if msg == 'q':
             print("Quitting...")
             break
-        server.send(bytes(msg, 'utf8'))
+        elif msg == 'u':
+            udp_sender()
+        else:
+            tcp_server.send(bytes(msg, 'utf8'))
         # print('You sent a message: ' + msg)
-    server.close()
+    tcp_server.close()
 
 
-def receiver():
-    while not can_receive_messages:
-        pass
-    print("Now you can use this chat!")
+def tcp_receiver():
     while True:
-        data = server.recv(1024)
+        data = tcp_server.recv(1024)
+        print(str(data, 'utf8'))
+
+
+def udp_sender():
+    udp_server.sendto(bytes(nickname, 'utf8'), (server_IP, PORT))
+
+    msg = input()
+    if msg == 'q':
+        print("Quitting...")
+
+    udp_server.sendto(bytes(msg, 'utf8'), (server_IP, PORT))
+
+
+def udp_receiver():
+    while True:
+        data, addr = udp_server.recvfrom(BUFFER_SIZE)
         print(str(data, 'utf8'))
 
 
 if __name__ == '__main__':
+    nickname = input('Give me your nickname: ')
+    print("Now you can use this chat!")
 
-    sending_thread = threading.Thread(target=sender)
-    receiving_thread = threading.Thread(target=receiver)
+    tcp_sending_thread = threading.Thread(target=tcp_sender)
+    tcp_receiving_thread = threading.Thread(target=tcp_receiver)
 
-    sending_thread.start()
-    receiving_thread.start()
+    udp_sending_thread = threading.Thread(target=udp_sender)
+    udp_receiving_thread = threading.Thread(target=udp_receiver)
+
+    tcp_sending_thread.start()
+    tcp_receiving_thread.start()
 
     while True:
+        sleep(5)
         pass
 
     # server.close()
--- Lab1/homework/PythonTcpServer.py
+++ Lab1/homework/PythonTcpServer.py
@@ -2,8 +2,9 @@ import socket
 import threading
 from time import sleep
 
-TCP_PORT = 8014
+PORT = 8015
 BUFFER_SIZE = 1024
+DEFAULT_NICKNAME = 'basic_nickname'
 
 
 def worker(address):
@@ -15,51 +16,76 @@ def worker(address):
         buff = cl.recv(BUFFER_SIZE)
 
         print(
-            "python udp server received msg: " + str(buff, 'utf8') + " from " + nickname)
+            "python tcp server received msg: " + str(buff, 'utf8') + " from " + nickname)
 
         if not buff:
             print(nickname + ' exits')
             break
 
-        msg_to_send = nickname + ": " + str(buff, 'utf8')
-        print("msg_to_send: " + msg_to_send)
-
-        recipients = [p for p in clients.keys() if p != address]
-        print("recipients: " + str(recipients))
-
-        for recip in recipients:
-            clients[recip][0].send(bytes(msg_to_send, 'utf8'))
-            print("Sent message: " + msg_to_send + " to " + str(recip))
+        send_message_to_all_clients_excluding(buff, nickname, address)
 
     del clients[address]
     cl.close()
 
 
-if __name__ == '__main__':
+def send_message_to_all_clients_excluding(buff, nickname, address):
+    msg_to_send = nickname + ": " + str(buff, 'utf8')
+    print("msg_to_send: " + msg_to_send)
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('', TCP_PORT))
-    server_socket.listen()
+    recipients = [p for p in clients.keys() if p != address]
+    print("recipients: " + str(recipients))
+
+    for recip in recipients:
+        clients[recip][0].send(bytes(msg_to_send, 'utf8'))
+        print("Sent message: " + msg_to_send + " to " + str(recip))
+
+
+def udp_worker():
+    nickname = DEFAULT_NICKNAME
+    while nickname == DEFAULT_NICKNAME:
+        pass
+
+    buff, addr = server_udp_socket.recvfrom(BUFFER_SIZE)
+    nickname = str(buff, 'utf8')
+
+    while True:
+        message, _ = server_udp_socket.recvfrom(BUFFER_SIZE)
+        print("python udp server received msg: " + str(buff, 'utf8'))
+        send_message_to_all_clients_excluding(message, nickname, addr)
+
+
+if __name__ == '__main__':
+    # open_tcp_connection
+    server_tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_tcp_socket.bind(('', PORT))
+    server_tcp_socket.listen()
+
+    # open_udp_connection
+    server_udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_udp_socket.bind(('', PORT))
+
+    udp_t = threading.Thread(target=udp_worker)
+    udp_t.start()
 
     clients = dict()  # address -> thread_no
 
-    print('PYTHON UDP SERVER')
+    print('PYTHON TCP SERVER')
 
     while True:
-        client_socket, address = server_socket.accept()
+        client_tcp_socket, address = server_tcp_socket.accept()
 
         print("New client: " + str(address[0]) + ":" + str(address[1]) + "!")
 
-        clients[address] = (client_socket, '')
+        clients[address] = (client_tcp_socket, DEFAULT_NICKNAME)
 
-        users = [c[1] for c in clients.values() if c[1] != '']
-        client_socket.send(bytes("Available users: " + str(users), 'utf8'))
+        users = [c[1] for c in clients.values() if c[1] != DEFAULT_NICKNAME]
+        client_tcp_socket.send(bytes("Available users: " + str(users), 'utf8'))
 
         t = threading.Thread(target=worker, args=(address,))
         t.start()
 
         sleep(2)
         if not len(clients):  # if all clients are gone
-            server_socket.close()
+            server_tcp_socket.close()
             break
 # todo: ctrl + C handling
