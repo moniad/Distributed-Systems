ff021f9249ee123c1d60aed76e0aa7940247be1b
moniad <moni.dziedzic@gmail.com>
2020-03-09 02:17:54 +0100

    Multicast works

--- Lab1/homework/Client.py
+++ Lab1/homework/Client.py
@@ -1,5 +1,6 @@
 import select
 import socket
+import struct
 
 import threading
 
@@ -7,12 +8,15 @@ import threading
 class MySocket:
     MSG_SIZE = 1024
     SERVER_IP = 'localhost'
+    MULTICAST_IP = '224.0.0.1'
     PORT = 8000
+    MULTICAST_PORT = 10001
     EPOLL_TIMEOUT = 5
 
     def __init__(self):
         self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.multicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def send_message_via_tcp(self, message):
         self.tcp_socket.send(bytes(message, 'utf8'))
@@ -25,6 +29,13 @@ class MySocket:
 
     def receive_message_via_udp(self):
         msg, addr = self.udp_socket.recvfrom(self.MSG_SIZE)
+        return str(msg, 'utf8'), addr
+
+    def send_message_via_multicast(self, message, addr):
+        self.multicast_socket.sendto(bytes(message, 'utf8'), addr)
+
+    def receive_message_via_multicast(self):
+        msg, addr = self.multicast_socket.recvfrom(self.MSG_SIZE)
         return str(msg, 'utf8'), addr
 
     def close_socket(self):
@@ -40,9 +51,15 @@ class ClientSocket(MySocket):
         self.e = select.epoll()
         fd = self.tcp_socket.fileno()
         self.e.register(fd, select.EPOLLIN)
+
         self.has_sent_via_udp = False
 
-        # self.channels[self.multicast_sock.fileno()] = self.multicast_sock todo
+        self.multicast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+        self.multicast_socket.bind(('', self.MULTICAST_PORT))
+
+        mreq = struct.pack('4sL', socket.inet_aton(self.MULTICAST_IP), socket.INADDR_ANY)
+        self.multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
         self.run()
 
@@ -57,9 +74,12 @@ class ClientSocket(MySocket):
         sender = threading.Thread(target=self.sender)
         poll_worker = threading.Thread(target=self.poll)
         udp_worker = threading.Thread(target=self.udp_receiver)
+        multicast_worker = threading.Thread(target=self.multicast_worker)
+
         sender.start()
         poll_worker.start()
         udp_worker.start()
+        multicast_worker.start()
 
     def sender(self):
         self.send_message_via_tcp(nickname)
@@ -81,8 +101,8 @@ class ClientSocket(MySocket):
                     print('[UDP] You sent a message: ' + msg)
                 elif msg == 'm':
                     msg = input('Give me your MULTICAST msg: ')
-                    # self.send_message_via_multicast(msg, (self.SERVER_IP, self.PORT))
-                    print('[M] You DID NOT SEND a message: ' + msg)
+                    self.send_message_via_multicast(msg, (self.MULTICAST_IP, self.MULTICAST_PORT))
+                    print('[M] You sent a message: ' + msg)
                 else:
                     self.send_message_via_tcp(msg)
                     print('[TCP] You sent a message: ' + msg)
@@ -103,6 +123,11 @@ class ClientSocket(MySocket):
         while True:
             msg, addr = self.receive_message_via_udp()
             print('[UDP] ' + msg)
+
+    def multicast_worker(self):
+        while True:
+            msg, addr = self.receive_message_via_multicast()
+            print('[MULTI] ' + msg)
 
 
 if __name__ == '__main__':
